feb58347c80773eb74d631d5ebe58ceca6a62670
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-14 00:21:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -40,9 +40,9 @@ class App < Sinatra::Base
     if @operation == "add"
       "#{@number1 + @number2}"
         elsif @operation == "subtract"
-        "#{@number1} - #{@number2}"
+        "#{@number1 - @number2}"
           elsif @operation == "multiply"
-            "#{@number1} * #{@number2}"
+            "#{@number1 * @number2}"
             elsif @operation == "divide"
             "#{@number1} / #{@number2}"
     end 
